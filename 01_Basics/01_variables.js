65618094fba23d2c4db37704af84bc3cca97d090
TanishTechPro <164313595+TanishTechPro@users.noreply.github.com>
2024-12-16 13:58:05 +0000

    var

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 144554
+let accountEmail = "tanish@gmail.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+
+
+accountEmail= "hcha@hc.com"
+accountPassword = "212231"
+accountCity = "Bengaluru"
+
+console.log(accountId);
+
+console.table([accountId,accountEmail,accountPassword,accountCity])
